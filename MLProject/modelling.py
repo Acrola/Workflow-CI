cf52796164d9d60411be750b5247a63f4f8b07f2
Acrola <kenny.aldebaran.roberts@gmail.com>
2025-06-10 19:30:14 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -44,7 +44,9 @@ def train_and_log_model(X_train, y_train, X_test, y_test, params, model_name="Ra
     mlflow.log_param("verbose", params.get("verbose", 0))
     mlflow.log_param("warm_start", params.get("warm_start", False))
 
-    model = RandomForestClassifier(**params)
+    params = params.copy()  # Avoid mutating the original
+    random_state = params.pop("random_state", 42)  # Remove and get random_state (default 42)
+    model = RandomForestClassifier(random_state=random_state, **params)
 
     # --- 2. Manually Log Training Time (1st Additional Metric) ---
     start_time = time.time()
